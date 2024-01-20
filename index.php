00c274ae472b827e18081032d78d39ca441942a2
Michelazani <michi.zani220@gmail.com>
2024-01-20 20:11:39 +0100

    aggiunta background color container

--- index.php
+++ index.php
@@ -19,32 +19,36 @@ Verificato il corretto funzionamento del nostro codice, spostiamo la logica in u
 </head>
 
 <body>
-
-
-<?php
-    require_once __DIR__ .'/../php-strong-password-generator/utilities/function.php';
-?>
+    <?php
+    require_once __DIR__ . '/../php-strong-password-generator/utilities/function.php';
+    ?>
     <header class="text-center">
         <h1>Strong Password Generator</h1>
         <h3>Genera una password sicura</h3>
     </header>
     <main>
-        <section class="password d-flex">
+        <section class="container d-flex flex-column p-4">
+            <div class="d-flex">
             <p>Lunghezza password:</p>
             <form action="./index.php" method="GET">
                 <label for="passwordLenght"></label>
                 <input type="text" name="passwordLenght" id="passwordLenght">
-                <input type="submit" value="Create">
+                <div class="mt-3 mb-3 ms-1">
+                    <button type="submit" class="btn btn-primary">Invia</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
+                </div>
             </form>
+            </div>
             <p> Your password is: <?php
-                       if (isset($_GET['passwordLenght'])) {
-                        $lenght = $_GET['passwordLenght'];
-                        $newPassword = generateRandomPassword($lenght);
-                        echo $newPassword;
-                    };
-            ?>  </p>
-        </section>
+                                    if (isset($_GET['passwordLenght'])) {
+                                        $lenght = $_GET['passwordLenght'];
+                                        $newPassword = generateRandomPassword($lenght);
+                                        echo $newPassword;
+                                    };
+                                    ?> 
+            </p>
 
+        </section>
     </main>
 </body>
 
